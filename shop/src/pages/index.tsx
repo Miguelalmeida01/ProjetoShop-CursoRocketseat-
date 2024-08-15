72a8c34e0a111251fbea84b9edc985f56aaaeed1
Miguelalmeida01 <miguelalmeidasa01@gmail.com>
2024-08-15 15:03:58 -0300

    começo

---
+++ shop/src/pages/index.tsx
@@ -0,0 +1,22 @@
+import { styled } from "@/styles";
+
+const Button = styled('button', {
+  backgroundColor: '$green500',
+  borderRadius: 8,
+  border: 0, 
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold',
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)'
+  },
+})
+
+export default function Home() {
+  return (
+    <Button><span>hello</span> word</Button>
+  );
+}
---
+++ shop/src/pages/product.tsx
@@ -0,0 +1,6 @@
+export default function Product() {
+    return (
+      <h1>hello word 2</h1>
+    );
+  }
+  
